7c2f9a63da445217825df48e585da1d6684d5e5c
EliGolam <elistasibusiness@gmail.com>
2022-11-16 07:03:59 +0100

    Switched SASS CSS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+@extends('layouts.app', ['title' => 'Laravel Blogs Welcome'])
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-    <title>Laravel Blog</title>
-</head>
-
-<body>
-    <h1>My Blog</h1>
-</body>
-</html>
+@section('content')
+    Welcome Page
+@endsection
