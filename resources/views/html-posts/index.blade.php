e24f55e86ff75c6532cbc4aebff833df225c48dd
EliGolam <elistasibusiness@gmail.com>
2022-11-16 11:35:04 +0100

    Added caching routes

--- resources/views/html-posts/index.blade.php
+++ resources/views/html-posts/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-
+    @foreach ($posts as $idx=>$post)
+        <section>
+            <?= $post ?>
+        </section>
+    @endforeach
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -24,11 +24,30 @@ Route::get('/', function () {
 | using html literals through wildcards using php filegetcontent
 */
 Route::get('/html-posts', function () {
-    return view('html-posts.index');
+    $idx = 1;
+    $posts = [];
+
+    $posts[] = file_get_contents(resource_path("views/html-posts/posts/example-post-1.html"));
+    $posts[] = file_get_contents(resource_path("views/html-posts/posts/example-post-2.html"));
+    $posts[] = file_get_contents(resource_path("views/html-posts/posts/example-post-3.html"));
+
+
+    return view('html-posts.index', compact('posts'));
 });
 
 Route::get('/html-posts/{post}', function ($slug) {
-    $post = file_get_contents(resource_path("views/html-posts/posts/{$slug}.html"));
+    // Find an html-post by its slug and pass it dynamically to a view called show
+
+    if (!file_exists($path = resource_path("views/html-posts/posts/{$slug}.html"))) {
+        /* Redirect if File doesn't exist */
+        // return redirect('/html-posts');
+
+        /* Abort 404 */
+        abort(404);
+    }
+
+    // Cache
+    $post = cache()->remember("html-posts.{$slug}", now()->addDay(), fn() => file_get_contents($path));
 
     return view('html-posts.show', compact('post'));
-});
+})->where('post', '([a-zA-Z1-9\-\+_&])+');
