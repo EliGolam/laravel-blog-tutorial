edae865113a2b2f19ead6f725fab2ffa58039f53
EliGolam <elistasibusiness@gmail.com>
2022-11-24 05:11:34 +0100

    Fixed Tailwind import issues

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 
 <title>Laravel From Scratch Blog</title>
-
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,13 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.webpackConfig({
-    stats: {
-         children: true
-    }
-});
-
-
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('tailwindcss'),
